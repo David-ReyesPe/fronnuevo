ffc8161c4396ea38e0252d86443040f645378706
JohanDevep <simontrujillo1999@gmail.com>
2023-11-02 18:44:07 -0500

    vistas funcionando

--- src/Componentes/Pages/Home/Home.jsx
+++ src/Componentes/Pages/Home/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React from "react";
+import Capacitaciones from "./Capacitaciones/Capacitaciones";
+import QuienesSomos from "./QuienesSomos/QuienesSomos";
+import Proposito from "./Proposito/Proposito";
+import MisionVision from "./Mision-Vision/Mision-Vision";
+import Aprendizaje from "./Aprendizaje/Aprendizaje";
+import Corporativo from "./Corporativo/Corporativo";
+import Beneficio from "./Beneficio/Beneficio";
+import Facturacion from "./Facturacion/Facturacion";
+import Carousel from "../../Carousel/Carousel";
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="di">
+      {/* error error */}
+      <Carousel />
+      <Capacitaciones />
+      <QuienesSomos />
+      <Corporativo />
+      <Proposito />
+      <MisionVision />
+      <Aprendizaje />
+      <Beneficio />
+      <Facturacion />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
